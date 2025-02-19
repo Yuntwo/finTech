8e7dd9f702cf07a75eacb6438dbaa161c47722b0
Lucy <Yuntwo@bupt.cn>
2025-02-19 17:31:15 +0800

    feat: add mysql and redis dao and model

---
+++ app/lottery/internal/dao/init.go
@@ -0,0 +1,21 @@
+package dao
+
+import (
+	"mall-go/app/lottery/internal/config"
+)
+
+func init() {
+	config, err := config.GetAppConfig()
+	if err != nil {
+		panic("failed to load data config: " + err.Error())
+	}
+
+	initMysql(config)
+}
+
+func Close() {
+	err := Db.Close()
+	if err != nil {
+		print("Error on closing dbService client.")
+	}
+}
